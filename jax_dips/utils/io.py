c73784c0276fde896083c43d04be07bf946b05bf
Onur Yarabras <oyararbas@ucsb.edu>
2026-06-03 22:25:26 -0700

    Fixed .vts saving issues

--- jax_dips/utils/io.py
+++ jax_dips/utils/io.py
@@ -84,7 +84,10 @@ def write_vtk_manual(gstate, field_dict, filename="results/manual_dump"):
     X, Y, Z = onp.meshgrid(gstate.x, gstate.y, gstate.z, indexing="ij")
     host_dict = {}
     for field_name in field_dict.keys():
-        host_dict[field_name] = onp.array(field_dict[field_name])
+        val = onp.array(field_dict[field_name])
+        if val.ndim == 1 and val.size == X.size:
+            val = val.reshape(X.shape)
+        host_dict[field_name] = val
     structuredToVTK(filename, X, Y, Z, pointData=host_dict)
 
 
